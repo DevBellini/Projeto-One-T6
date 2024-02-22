8e8b93d3580f504c4cbeff4a66d31bb0338caaba
Guilherme Bellini <140752834+DevBellini@users.noreply.github.com>
2024-02-22 11:54:15 -0300

    alguns ajustes no css e html

---
+++ html/js/index.js
@@ -0,0 +1,15 @@
+let criptografia = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+];
+
+function btnCriptografar() {
+
+}
+
+function criptograr() {
+
+}
