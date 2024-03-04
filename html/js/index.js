da2ceb2cdeaeb6aee71082c67d905dd7e827dcc4
Guilherme Bellini <140752834+DevBellini@users.noreply.github.com>
2024-03-04 11:17:41 -0300

    atualizações

--- html/js/index.js
+++ html/js/index.js
@@ -1,15 +1,58 @@
-let criptografia = [
-    ['e', 'enter'],
-    ['i', 'imes'],
-    ['a', 'ai'],
-    ['o', 'ober'],
-    ['u', 'ufat']
-];
+const textArea = document.querySelector('.text-area');
+const mensagem = document.querySelector('.mensagem');
+
 
 function btnCriptografar() {
-
+    const textoEncriptado = criptografar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = '';
 }
 
-function criptograr() {
+function criptografar(stringEncriptada) {
+    let criptografia = [
+        ['e', 'enter'],
+        ['i', 'imes'],
+        ['a', 'ai'],
+        ['o', 'ober'],
+        ['u', 'ufat']
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
 
+    for (i = 0; i < criptografia.length; i++) {
+        if (stringEncriptada.includes(criptografia[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(criptografia[i][0], criptografia[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDescriptografar() {
+    const textoDesencriptado = descriptografar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = '';
+}
+
+
+function descriptografar(stringDesencriptada) {
+    let descriptografia = [
+        ['e', 'enter'],
+        ['i', 'imes'],
+        ['a', 'ai'],
+        ['o', 'ober'],
+        ['u', 'ufat']
+    ];
+
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (i = 0; i < descriptografia.length; i++) {
+        if (stringDesencriptada.includes(descriptografia[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(descriptografia[i][1], descriptografia[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function btnCopiar() {
+    const textoMensagem = mensagem.value;
+    console.log(textoMensagem);
 }
